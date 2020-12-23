e7474f855901e1cff76f042a3840738ef4625847
Long <longvanha@gmail.com>
2020-12-23 11:24:41 +0000

    update

--- scripts/Theme.js
+++ scripts/Theme.js
@@ -1,23 +1,66 @@
 document.addEventListener('DOMContentLoaded', () => {
-    productCarousel();
-    CBDCarousel();
+  productCarousel();
+  CBDCarousel();
+  burgerMenu();
+  stickyNavbar();
 })
 
 const productCarousel = () => {
     $(".home-product-carousel_wrap").owlCarousel({
-        center: true,
-        loop: true,
-        items: 3,
+      center: true,
+      margin: 20,
+      loop: true,
+      autoWidth: true,
+      items: 3,
       dots: true
     });
 }
 
 const CBDCarousel = () => {
     $(".home-cbd__carousel").owlCarousel({
-      center: true,
+      center: false,
+      margin: 20,
       items: 4,
-        dots: true,
-        loop: true,
-      autoplay:true
+      autoWidth: true,
+      loop: false,
+      autoplay: true,
+      dots: true
     });
+}
+
+
+const burgerMenu = () => {
+  const menuBtn = document.querySelector(".menu-btn");
+  const menuNav = document.querySelector(".site-header__nav__menu");
+  let menuOpen = false;
+
+  menuBtn.addEventListener("click", () => {
+    if (!menuOpen) {
+      menuBtn.classList.add("open");
+      menuOpen = true;
+    } else {
+      menuBtn.classList.remove("open");
+      menuOpen = false;
+    }
+
+    menuNav.classList.contains("show-menu")
+      ? menuNav.classList.remove("show-menu")
+      : menuNav.classList.add("show-menu"); 
+  });
+}
+
+const stickyNavbar = () => {
+  if ($(window).width() > 480) {
+    $(window).scroll(function () {
+      if ($(window).scrollTop() >= 28) {
+        $(".site-header").addClass("fixed-header");
+        $(".site-header__nav__menu").addClass("close-gap");
+        $("main").addClass("top-padding");
+      } else {
+        $(".site-header").removeClass("fixed-header");
+        $(".site-header__nav__menu").removeClass("close-gap");
+        $("main").removeClass("top-padding");
+      }
+    });
+  }
 }
